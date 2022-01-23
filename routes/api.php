44db3ccfbb6ae96948a91fcc1d4e2ce076de606c
Nitin Daddikar <clarus.nitin@gmail.com>
2022-01-23 11:13:48 +0530

    API_productgroup all

--- routes/api.php
+++ routes/api.php
@@ -112,6 +112,9 @@ Route::namespace('V1')->middleware(['cors'])->group(function() {
             Route::get('GetStockdetail', 'StockMasterController@GetStockdetail');
             Route::post('full_stock', 'StockMasterController@full_stock');
         });
+        Route::prefix("API_productgroup")->group(function () {
+            Route::get('all-product-group', 'ProductGroupController@all_list');
+        });
         Route::prefix("API_bank")->group(function () {
             Route::get('bank_details', 'LabelController@bank_details');
         });
