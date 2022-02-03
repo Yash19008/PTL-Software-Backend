980a3b692f47fc45f9c3482179b4636cd1073b36
Nitin Daddikar <clarus.nitin@gmail.com>
2022-02-03 10:21:33 +0530

    history changes

--- app/Http/Controllers/V1/Operations/OrderMasterController.php
+++ app/Http/Controllers/V1/Operations/OrderMasterController.php
@@ -101,7 +101,7 @@ class OrderMasterController extends Controller {
         $historyID = null;
         try {
             $customer = CustomerMaster::where("id", $request->get('customer_id'))->where('active', "1")->first();
-            $customer_ids = CustomerMaster::where("mobile", $customer->mobile)->pluck('customer_id')->toArray();
+            $customer_ids = CustomerMaster::where("mobile", $customer->mobile)->pluck('id')->toArray();
             $history = SearchHistoryMaster::whereIn("customer_id", $customer_ids)->orderBy('timestamp', 'DESC')->first();
             $historyID = $history->id;
         } catch (\Exception $e) {
@@ -118,7 +118,7 @@ class OrderMasterController extends Controller {
             "product_group" => $request->get('product_group'),
             "qty" => $request->get('qty'),
             "delivery_at" => $request->get('deliv'),
-            "last_searched_id" => $history->id,
+            "last_searched_id" => $historyID,
             "updated_dt" => date('Y-m-d H:i:s'),
             "update_by" => $request->get('customer_id'),
         );
