8322d9b2c52fe24fd55498a64774bc03e4df47a4
Nitin Daddikar <clarus.nitin@gmail.com>
2022-06-05 11:15:46 +0530

    search logic, excel and mysql table

--- app/Http/Controllers/V1/Operations/SearchSizeController.php
+++ app/Http/Controllers/V1/Operations/SearchSizeController.php
@@ -81,6 +81,31 @@ class SearchSizeController extends Controller {
                 
                 group by quality, gsm, Size_INCH
                 ORDER BY utiliz DESC,gsm");
+                
+            $peptek_result = DB::select("
+                    SELECT 'PTL Vendor' as company, stock_vendors.product_group, stock_vendors.quality, gsm, dup.utiliz, dup.id, 
+                    CONCAT(size_inch_length,' X ',size_inch_width) as Size_INCH,size_inch_length ,size_inch_width,
+                    TRUNCATE(size_inch_length/".$userlength." ,0) AS LEN_UPS ,
+                    TRUNCATE(size_inch_width/".$userwidth." ,0) AS WID_UPS ,
+                    TRUNCATE(TRUNCATE(size_inch_length/".$userlength.",0)*TRUNCATE(size_inch_width/".$userwidth.",0),0) AS total_ups,
+                    TRUNCATE((pkt_grs_weight/sheet*100),1)  as  sheet_weight,
+                    (sheet*pkg_mode) as bundle,
+                    
+                    SUM(sheet*pkt_grs) as total_sheet
+                    
+                    FROM stock_vendors
+                    
+                    INNER JOIN
+                    (SELECT id, (ROUND(((".$userlength."*".$userwidth.")/(size_inch_length*size_inch_width))*(TRUNCATE(TRUNCATE(size_inch_length/".$userlength.",0)*TRUNCATE(size_inch_width/".$userwidth.",0),0)) * 100)) as utiliz
+                    FROM stock_vendors
+                    WHERE stock_vendors.gsm BETWEEN ".$lower_range." AND ".$upper_range."
+                    HAVING  utiliz >= (SELECT op.value FROM options_master op WHERE op.option='utilization_ups_admin') AND utiliz <= 100 ORDER BY utiliz DESC, size_inch_length ASC) dup
+                    ON stock_vendors.id = dup.id
+                    
+                    ".$where."
+                    
+                    group by quality, gsm, Size_INCH
+                    ORDER BY utiliz DESC,gsm");
         $output = array_merge($result, $peptek_result);
         return $this->success('Search Size Responses List', $output, 200);
     }
@@ -144,23 +169,23 @@ class SearchSizeController extends Controller {
         // insert query for maintaining history for how search made by particular person
         
         $data=array(
-                    "customer_id" => $result_cust->id,
-                    "company_name" => $result_cust->company_name,
-                    "client_name" => $result_cust->client_name,
-                    "email" => $result_cust->email,
-                    "mobile" => $result_cust->mobile,
-                    "width" => $userwidth,
-                    "heigth" => $userlength,
-                    "size_in_inch" => $size_in_inch,
-                    "gsm" => $gsm,
-                    "product_group" => $product_group,
-                    "timestamp" => date('Y-m-d H:i:s')
-        );
+            "customer_id" => $result_cust->id,
+            "company_name" => $result_cust->company_name,
+            "client_name" => $result_cust->client_name,
+            "email" => $result_cust->email,
+            "mobile" => $result_cust->mobile,
+            "width" => $userwidth,
+            "heigth" => $userlength,
+            "size_in_inch" => $size_in_inch,
+            "gsm" => $gsm,
+            "product_group" => $product_group,
+            "timestamp" => date('Y-m-d H:i:s')
+);
+
+// insert into search history
+SearchHistoryMaster::create($data);
         
-        // insert into search history
-        SearchHistoryMaster::create($data);
-                
-        $option_result = OptionMaster::where('option', 'gsm_range')->first();
+$option_result = OptionMaster::where('option', 'gsm_range')->first();
         $gsm_range = $option_result->value;
         $upper_range = $gsm + $gsm_range;
         $lower_range = $gsm - $gsm_range;
@@ -221,6 +246,36 @@ class SearchSizeController extends Controller {
                     HAVING  utilization >= (SELECT op.value FROM options_master op WHERE op.option='utilization_ups') AND utilization <= 100 
                     ORDER BY utilization DESC, size_inch_length ASC) util
                 ON peptek_stock.id = util.id
+                
+                ".$where."
+                
+                GROUP BY quality, gsm, Size_INCH
+                
+                
+                UNION
+                
+                
+                SELECT 'PTL Vendor' as company, stock_vendors.product_group, stock_vendors.quality, gsm, util.utilization, stock_vendors.id,
+                CONCAT(size_inch_length,' X ',size_inch_width) as Size_INCH,size_inch_length ,size_inch_width,
+                CONCAT(size_inch_length,' X ',size_inch_width) as size_inch,
+                TRUNCATE(size_inch_length/".$userlength." ,0) AS LEN_UPS ,
+                TRUNCATE(size_inch_width/".$userwidth." ,0) AS WID_UPS ,
+                TRUNCATE(TRUNCATE(size_inch_length/".$userlength.",0)*TRUNCATE(size_inch_width/".$userwidth.",0),0) AS total_ups,
+                TRUNCATE((pkt_grs_weight/sheet*100),1)  as  sheet_weight,
+                (sheet*pkg_mode) as bundle,
+                
+                SUM(sheet*pkt_grs) as total_sheet
+                
+                FROM stock_vendors 
+                
+                
+                INNER JOIN
+                    (SELECT id, (ROUND(((".$userlength."*".$userwidth.")/(size_inch_length*size_inch_width))*(TRUNCATE(TRUNCATE(size_inch_length/".$userlength.",0)*TRUNCATE(size_inch_width/".$userwidth.",0),0)) * 100)) as utilization
+                    FROM stock_vendors
+                    WHERE stock_vendors.gsm BETWEEN ".$lower_range." AND ".$upper_range." 
+                    HAVING  utilization >= (SELECT op.value FROM options_master op WHERE op.option='utilization_ups') AND utilization <= 100 
+                    ORDER BY utilization DESC, size_inch_length ASC) util
+                ON stock_vendors.id = util.id
                 
                 ".$where."
                 
