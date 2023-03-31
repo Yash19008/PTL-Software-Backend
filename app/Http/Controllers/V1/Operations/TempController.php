ee3f7eee339fa65d6d674114825bb000e35821df
devika2403 <devika2403@gmail.com>
2023-03-31 15:52:42 +0530

    temp import changed

--- app/Http/Controllers/V1/Operations/TempController.php
+++ app/Http/Controllers/V1/Operations/TempController.php
@@ -8,7 +8,11 @@ use Illuminate\Http\Request;
 class TempController extends Controller
 {
     public function temp_stock_import(Request $request){
-        echo '<pre>';print_r($request->all());echo '</pre>';exit();
+        if(!empty($request->all())){
+            return $this->success('Import outside stock successfully !!', $request->all(), 200);
+        }else{
+            return $this->failure('Something Went Wrong !!', $e->getMessage(), 500);
+        }
         
     }
 }
