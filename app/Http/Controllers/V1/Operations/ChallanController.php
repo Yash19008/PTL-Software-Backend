4e78fcfab3158666b2128595f91a0db006b6e9b8
devika2403 <devika2403@gmail.com>
2023-04-25 12:27:03 +0530

    new api for nested challan

--- app/Http/Controllers/V1/Operations/ChallanController.php
+++ app/Http/Controllers/V1/Operations/ChallanController.php
@@ -192,6 +192,68 @@ class ChallanController extends Controller {
             return $this->failure('Something Went Wrong !!', $e->getMessage(), 500);
         }
     }
+    public function import_challan_outside_nested(Request $request){
+       
+        //ADD STATUS AS "PENDING"/"DELIVERED"
+         try {
+            //$challanArr = json_decode($request->challanObj, true);
+            if(!empty($request->all())){
+                foreach ($request->all() as $key => $value) {
+                    $challan = ChallanList::where('challan_no',$value['challan_no'])->get();
+                    if(count($challan) == 0){
+                        foreach ($value['items'] as $key => $val) {
+                            $input = [
+                                "customer_name"=> $value['customer_name'],
+                                "mobile"=>$value['mobile'],
+                                "date" => date('Y-m-d', strtotime($value['date'])),
+                                "challan_no"=> $value['challan_no'],
+                                "delivery_at"=>$value['delivery_at'],
+                                "status"=>$value['status'],
+                                "quality"=>$val['quality'],
+                                "size_inch_length"=>$val['size_inch_length'],
+                                "size_inch_width"=>$val['size_inch_width'],
+                                "gsm"=>$val['gsm'],
+                                "bdls"=> $val['bdls'],
+                                "pkt_grs"=>$val['pkt_grs'],
+                                "sheets"=>$val['sheets'],
+                                "weight"=>$val['weight'],
+                            ];
+                            ChallanList::create($input);
+                        }
+
+                    }else{
+                        ChallanList::where('challan_no',$value['challan_no'])->delete();
+                        foreach ($value['items'] as $key => $val) {
+                            $input = [
+                                "customer_name"=> $value['customer_name'],
+                                "mobile"=>$value['mobile'],
+                                "date" => date('Y-m-d', strtotime($value['date'])),
+                                "challan_no"=> $value['challan_no'],
+                                "delivery_at"=>$value['delivery_at'],
+                                "status"=>$value['status'],
+                                "quality"=>$val['quality'],
+                                "size_inch_length"=>$val['size_inch_length'],
+                                "size_inch_width"=>$val['size_inch_width'],
+                                "gsm"=>$val['gsm'],
+                                "bdls"=> $val['bdls'],
+                                "pkt_grs"=>$val['pkt_grs'],
+                                "sheets"=>$val['sheets'],
+                                "weight"=>$val['weight'],
+                            ];
+                            ChallanList::create($input);
+                        }
+                      
+                    }
+                }
+                return $this->success('Import outside challan successfully !!', $request->all(), 200);
+              
+            }else{
+                return $this->failure('Empty data found or Data not proper !!', 500);
+            }
+        } catch (\Exception $e) {
+            return $this->failure('Something Went Wrong !!', $e->getMessage(), 500);
+        }
+    }
     public function deleteAllChallan(){
         ChallanList::truncate();
         return $this->success('Challan table truncated successfully !!', 200);
