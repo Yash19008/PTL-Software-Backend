687667b7825b2d722cd3c16de0ffc69da17ea5f5
devika2403 <devika2403@gmail.com>
2023-04-25 15:38:07 +0530

    change in date unique related

--- app/Http/Controllers/V1/Operations/ChallanController.php
+++ app/Http/Controllers/V1/Operations/ChallanController.php
@@ -199,7 +199,8 @@ class ChallanController extends Controller {
             //$challanArr = json_decode($request->challanObj, true);
             if(!empty($request->all())){
                 foreach ($request->all() as $key => $value) {
-                    $challan = ChallanList::where('challan_no',$value['challan_no'])->get();
+                    $challan = ChallanList::where('challan_no',$value['challan_no'])
+                    ->where('date', date('Y-m-d', strtotime($value['date'])))->get();
                     if(count($challan) == 0){
                         foreach ($value['items'] as $key => $val) {
                             $input = [
