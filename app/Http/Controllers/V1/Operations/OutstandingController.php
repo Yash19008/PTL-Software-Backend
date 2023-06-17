c932d3c0799656b4b432a11512727bd05659c1d1
devika2403 <devika2403@gmail.com>
2023-06-17 14:11:42 +0530

    change in outstanding api structure

--- app/Http/Controllers/V1/Operations/OutstandingController.php
+++ app/Http/Controllers/V1/Operations/OutstandingController.php
@@ -229,39 +229,36 @@ class OutstandingController extends Controller {
             //$outstandingArr = json_decode($request->outstandingObj, true);
             if(!empty($request->all())){
                 foreach ($request->all() as $key => $value) {
-                    $outstanding = Outstanding::where('voucher_no',$value['voucher_no'])
-                    ->where('date', date('Y-m-d', strtotime($value['date'])))->get();
+                    foreach ($value['items'] as $key => $val) {
+                        $outstanding = Outstanding::where('voucher_no',$val['voucher_no'])
+                        ->where('date', date('Y-m-d', strtotime($val['date'])))->get();
                         if(count($outstanding) == 0){
-                           
-                            foreach ($value['items'] as $key => $val) {
-                                $input = [
-                                    "date" =>date('Y-m-d', strtotime($value['date'])),
-                                    "customer_name"=> $value['customer_name'],
-                                    "mobile"=>$val['mobile'],
-                                    "email"=>$val['email'],
-                                    "voucher_no"=>$value['voucher_no'],
-                                    "voucher_type"=>$val['voucher_type'],
-                                    "credit_days"=> $val['credit_days'],
-                                    "due_date"=>NULL,
-                                    "over_dues"=>NULL,
-                                    "total_amount"=>$val['total_amount'],
-                                    "part_paid"=>$val['part_paid'],
-                                    "balance"=>$val['balance'],
-                                    'updated_on'=>Carbon::now(),
-                                ];
-                                Outstanding::create($input);
-                            }
-                          
+                            $input = [
+                                "date" =>date('Y-m-d', strtotime($val['date'])),
+                                "customer_name"=> $value['customer_name'],
+                                "mobile"=>$value['mobile'],
+                                "email"=>$value['email'],
+                                "voucher_no"=>$val['voucher_no'],
+                                "voucher_type"=>$val['voucher_type'],
+                                "credit_days"=> $val['credit_days'],
+                                "due_date"=>NULL,
+                                "over_dues"=>NULL,
+                                "total_amount"=>$val['total_amount'],
+                                "part_paid"=>$val['part_paid'],
+                                "balance"=>$val['balance'],
+                                'updated_on'=>Carbon::now(),
+                            ];
+                            Outstanding::create($input);
                         }else{
-                           
-                            Outstanding::where('voucher_no',$value['voucher_no'])->delete();
+                    
+                            Outstanding::where('voucher_no',$val['voucher_no'])->delete();
                             foreach ($value['items'] as $key => $val) {
                                 $input = [
-                                    "date" =>date('Y-m-d', strtotime($value['date'])),
+                                    "date" =>date('Y-m-d', strtotime($val['date'])),
                                     "customer_name"=> $value['customer_name'],
-                                    "mobile"=>$val['mobile'],
-                                    "email"=>$val['email'],
-                                    "voucher_no"=>$value['voucher_no'],
+                                    "mobile"=>$value['mobile'],
+                                    "email"=>$value['email'],
+                                    "voucher_no"=>$val['voucher_no'],
                                     "voucher_type"=>$val['voucher_type'],
                                     "credit_days"=> $val['credit_days'],
                                     "over_dues"=>NULL,
@@ -273,8 +270,9 @@ class OutstandingController extends Controller {
                                 ];
                                 Outstanding::create($input);
                             }
+                        }
                     }
-                   
+                    
                 }
                 return $this->success('Import outside outstanding successfully !!', $request->all(), 200);
             }else{
