cb56dcccd9a1b29901b96eea33121d81d862565d
devika2403 <devika2403@gmail.com>
2023-06-23 15:30:27 +0530

    delete single challan api

--- app/Http/Controllers/V1/Operations/ChallanController.php
+++ app/Http/Controllers/V1/Operations/ChallanController.php
@@ -315,5 +315,29 @@ class ChallanController extends Controller {
             return $this->failure('Something Went Wrong !!', $e->getMessage(), 500);
         }
     }
+    public function deleteChallan(Request $request){
+        // VALIDATION RULE
+        $validation_array = array(
+            'date' => 'required|date_format:Y-m-d',
+            'challan_no' => 'required',
+        );
+        $rules = [
+            'date.required' => 'The Date is required.',
+            'challa_no.required' => 'Challan no is required.',
+            'date.date_format' => 'Please pass Y-m-d format for date.',
+        ];
+
+        // CHECK SERVER SIDE VALIDATION
+        $this->validate($request, $validation_array, $rules);
+        $challan = ChallanList::where('challan_no',$request->challan_no)->where('date',$request->date)->get();
+      
+        if(count($challan) > 0){
+            ChallanList::where('challan_no',$request->challan_no)->where('date',$request->date)->delete();
+            return $this->success('Challan has been deleted successfully !!', 200);
+        }else{
+            return $this->failure('Challan number or date not found !!', 500);
+        }
+       
+    }
 
 }
