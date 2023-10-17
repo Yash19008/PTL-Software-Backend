d0faeee2d74009943aad8458900e28995d68535f
Nitin Daddikar <clarus.nitin@gmail.com>
2023-10-17 17:24:29 +0530

    added company as a column

--- app/Http/Controllers/V1/Operations/OrderMasterController.php
+++ app/Http/Controllers/V1/Operations/OrderMasterController.php
@@ -64,7 +64,7 @@ class OrderMasterController extends Controller
 
     public function getTableColumn()
     {
-        return array("id" => "id", "date" => "date", "company_name" => "company_name", "quality" => "quality", "size_inch_length" => "size_inch_length", "size_inch_width" => "size_inch_width", "gsm" => "gsm", "qty" => "qty", "delivery_at" => "delivery_at", "status" => "status", "challan_number" => "challan_number", "gwd" => "gwd");
+        return array("id" => "id", "date" => "date", "company_name" => "company_name", "quality" => "quality", "size_inch_length" => "size_inch_length", "size_inch_width" => "size_inch_width", "gsm" => "gsm", "qty" => "qty", "delivery_at" => "delivery_at", "status" => "status", "challan_number" => "challan_number", "gwd" => "gwd", "company" => "company");
     }
 
     public function find_company_list(Request $request)
