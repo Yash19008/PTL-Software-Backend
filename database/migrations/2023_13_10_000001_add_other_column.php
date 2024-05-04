487f0073a266de3ace8aa61eaca3230eb3b63d23
Nitin Daddikar <clarus.nitin@gmail.com>
2024-05-04 11:29:33 +0530

    changes

--- database/migrations/2023_13_10_000001_add_other_column.php
+++ database/migrations/2023_13_10_000001_add_other_column.php
@@ -30,7 +30,7 @@ class AddOtherColumn extends Migration
 
         DB::statement("CREATE OR REPLACE VIEW order_master_view AS
                         select om.id, om.date, om.cust_id, cm.company_name, om.quality, om.size_inch_length, om.size_inch_width , om.gsm, om.qty, om.weight, om.delivery_at, om.challan_number,om.gwd, om.last_searched_id
-                        , om.challan_bulk_action_date, om.challan_id, om.pkt_grs_weight, om.sheet, om.pkg_mode, om.pkt_grs, om.loc, om.bdls, om.size_cms_length, om.size_cms_width, om.company,
+                        , om.challan_bulk_action_date, om.challan_id, om.pkt_grs_weight, om.sheet, om.pkg_mode, om.pkt_grs, om.loc, om.bdls, om.size_cms_length, om.size_cms_width, om.company, om.is_reel,
                          if(om.status = 'P','Pending',if(om.status = 'B','Booked',if(om.status = 'NS','Not In Stock','Cancel'))) as status 
                         from order_master om
                         left join customer_master cm on om.cust_id=cm.id");
