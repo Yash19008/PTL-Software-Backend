70200a26c05139c6e02885ceb278ae1e30a190d6
Nitin Daddikar <clarus.nitin@gmail.com>
2025-10-11 15:40:21 +0530

    eta in StockMaster

--- app/Models/V1/Operations/StockMaster.php
+++ app/Models/V1/Operations/StockMaster.php
@@ -11,6 +11,6 @@ class StockMaster extends Model
     public $timestamps = false;
     
     protected $fillable = [
-        'id', 'product_group', 'gsm' , 'size_inch_length', 'size_inch_width', 'size_cms_length', 'size_cms_width', 'pkt_grs_weight', 'sheet', 'bdls', 'pkt_grs', 'pkg_mode', 'weight', 'quality', 'gwd', 'loc', 'updated_on'
+        'id', 'product_group', 'gsm' , 'size_inch_length', 'size_inch_width', 'size_cms_length', 'size_cms_width', 'pkt_grs_weight', 'sheet', 'bdls', 'pkt_grs', 'pkg_mode', 'weight', 'quality', 'gwd', 'loc', 'updated_on', 'eta'
     ];
 }
