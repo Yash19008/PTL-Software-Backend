cba493e9a4ea4b2ac36975322684cfbb5d6c237e
hardcraft123 <hardik.thecraftpiece@gmail.com>
2026-02-02 10:47:44 +0530

    fixed syntax issue in column

--- database/migrations/2026_02_02_104328_add_max_allowed_devices_to_customer_master.php
+++ database/migrations/2026_02_02_104328_add_max_allowed_devices_to_customer_master.php
@@ -4,14 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDeviceInfoAndPlatformColumns extends Migration
+class AddMaxAllowedDevicesAndPlatformColumns extends Migration
 {
     public function up()
     {
+        // customer_master
         if (Schema::hasTable('customer_master')) {
             Schema::table('customer_master', function (Blueprint $table) {
                 if (!Schema::hasColumn('customer_master', 'max_allowed_devices')) {
-                    $table->json('max_allowed_devices')->nullable()->after('mobile_show_stocks_from');
+                    $table->unsignedTinyInteger('max_allowed_devices')
+                          ->default(2)
+                          ->after('mobile_show_stocks_from');
                 }
             });
         }
