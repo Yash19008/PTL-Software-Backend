c01e887398f232ec64701bebf8b92b359eb397c3
hardcraft123 <hardik.thecraftpiece@gmail.com>
2026-02-02 10:44:09 +0530

    add new migration fiel

---
+++ database/migrations/2026_02_02_104328_add_max_allowed_devices_to_customer_master.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDeviceInfoAndPlatformColumns extends Migration
+{
+    public function up()
+    {
+        if (Schema::hasTable('customer_master')) {
+            Schema::table('customer_master', function (Blueprint $table) {
+                if (!Schema::hasColumn('customer_master', 'max_allowed_devices')) {
+                    $table->json('max_allowed_devices')->nullable()->after('mobile_show_stocks_from');
+                }
+            });
+        }
+
+        // search_history_master
+        if (Schema::hasTable('search_history_master')) {
+            Schema::table('search_history_master', function (Blueprint $table) {
+                if (!Schema::hasColumn('search_history_master', 'platform')) {
+                    $table->enum('platform', ['mobile', 'web'])
+                          ->default('mobile')
+                          ->after('product_group');
+                }
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::table('customer_master', function (Blueprint $table) {
+            if (Schema::hasColumn('customer_master', 'max_allowed_devices')) {
+                $table->dropColumn('max_allowed_devices');
+            }
+        });
+
+        Schema::table('search_history_master', function (Blueprint $table) {
+            if (Schema::hasColumn('search_history_master', 'platform')) {
+                $table->dropColumn('platform');
+            }
+        });
+    }
+}
