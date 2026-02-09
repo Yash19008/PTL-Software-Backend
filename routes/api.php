8825a19735538d5fe4ee3895386523c932b40493
suraj pandey <surajpandey@surajs-MacBook-Air.local>
2026-02-09 22:37:10 +0530

    create new logout api

--- routes/api.php
+++ routes/api.php
@@ -120,6 +120,7 @@ Route::namespace('V1')->middleware(['cors'])->group(function() {
         Route::prefix("API_login")->group(function () {
             Route::get('get_details', 'LoginController@get_details');
             Route::post('login_new', 'LoginController@login_new');
+            Route::post('logout_new', 'LoginController@logout_new');
             Route::get('verify_no_new', 'LoginController@verify_no_new');
             Route::get('SendSMS/{mobile_no}', 'LoginController@SendSMS');
             Route::get('verify_otp', 'LoginController@verify_otp');
