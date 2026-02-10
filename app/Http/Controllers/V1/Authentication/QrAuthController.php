3e4969fded899e9591e43f5728954d3cd9c7d70b
suraj pandey <surajpandey@surajs-MacBook-Air.local>
2026-02-11 01:50:54 +0530

    Solve Logged in issue

--- app/Http/Controllers/V1/Authentication/QrAuthController.php
+++ app/Http/Controllers/V1/Authentication/QrAuthController.php
@@ -67,7 +67,7 @@ class QrAuthController extends Controller
         }
 
         // 4️⃣ Fetch user
-        $user = CustomerMaster::find($request->user_id);
+        $user = UserMaster::find($request->user_id);
         if (!$user) {
             return $this->failure('User not found', null, 404);
         }
